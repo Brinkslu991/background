ef3c58f603abf1cf316281b7cb2e166d3ac7a39a
Brinkslu991 <153300807+Brinkslu991@users.noreply.github.com>
2025-03-05 11:08:56 -0500

    made code

--- main.py
+++ main.py
@@ -4,6 +4,17 @@ import pygame
 import sys
 import config # Import the config module
 
+# Draw boat
+def draw_ball(screen):
+    pygame.draw.ellipse(screen, config.BROWN, (200,300, 300,150))
+    pygame.draw.ellipse(screen, config.TAN, (270, 320, 10, 100))
+    pygame.draw.ellipse(screen, config.TAN, (310, 320, 10, 100))
+    pygame.draw.ellipse(screen, config.TAN, (350, 320, 10, 100))
+    pygame.draw.ellipse(screen, config.TAN, (390, 320, 10, 100))
+    pygame.draw.ellipse(screen, config.TAN, (430, 320, 10, 100))
+
+text_font = pygame.font.SysFont('Comic Sans MS', 30)
+
 def init_game ():
     pygame.init()
     screen = pygame.display.set_mode((config.WINDOW_WIDTH, config.WINDOW_HEIGHT)) # Use constants from config
@@ -18,13 +29,24 @@ def handle_events ():
             if event.key == pygame.K_ESCAPE:
                 return False
     return True
+
 def main():
+
+    def draw_text(text, font, text_col, x, y):
+        img = font.render(text, True, text_col)
+        screen.blit(img, (x,y))
+
     screen = init_game()
     clock = pygame.time.Clock() # Initialize the clock here
     running = True
     while running:
         running = handle_events()
-        screen.fill(config.WHITE) # Use color from config
+        screen.fill(config.BLUEGAMBIT) # Use color from config
+
+        draw_text('Bread', text_font, (0,0,0), 220, 150)
+
+        draw_ball(screen)
+
         pygame.display.flip()
         
         # Limit the frame rate to the specified frames per second (FPS)
