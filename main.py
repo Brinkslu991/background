5000d5593d679f4c0d0b2e0aed7d5a034b9c9e61
Brinkslu991 <153300807+Brinkslu991@users.noreply.github.com>
2025-03-07 10:58:49 -0500

    table

--- main.py
+++ main.py
@@ -13,6 +13,14 @@ def draw_ball(screen):
     pygame.draw.ellipse(screen, config.TAN, (390, 320, 10, 100))
     pygame.draw.ellipse(screen, config.TAN, (430, 320, 10, 100))
 
+def draw_table(screen):
+    pygame.draw.line(screen, config.ALMONDBROWN, (100, 450), (600, 450),10)
+    pygame.draw.line(screen, config.ALMONDBROWN, (150, 450), (150, 700),10)
+    pygame.draw.line(screen, config.DARK_TAN, (175, 456), (175, 700),10)
+    pygame.draw.line(screen, config.DARK_TAN, (525, 456), (525, 700),10)
+    pygame.draw.line(screen, config.ALMONDBROWN, (550, 450), (550, 700),10)
+
+
 
 
 def init_game ():
@@ -44,11 +52,13 @@ def main():
     running = True
     while running:
         running = handle_events()
-        screen.fill(config.BLUEGAMBIT) # Use color from config
+        screen.fill(config.WHITE) # Use color from config
 
-        draw_text('Bread', text_font, (110,110,110), 250, 200)
+        draw_text('Bread', text_font, (0,0,0), 250, 200)
 
         draw_ball(screen)
+
+        draw_table(screen)
 
         pygame.display.flip()
         
